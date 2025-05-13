5b2d1d936f9d1e7a048f09dd569796d85cb5d273
Miika <kall.miika@gmail.com>
2025-05-14 00:48:04 +0300

    Misc

--- plotit.py
+++ plotit.py
@@ -1,16 +1,18 @@
+import time
 from pprint import pprint
 from datetime import datetime
+from typing import TypeAlias, Literal
 
 import pandas as pd
 import plotly.express as px
 
 import geodata
-from geodata import ForecastQuery, AnalysisQuery
+from geodata import ForecastQuery, AnalysisQuery, GeoJSON
 
 
 
-def show(df, geojson, locations, color):
-    color_max = min(df.iloc[:,2].max(), 40)
+def get_figure(df, geojson, locations, color, variable):
+    color_max = min(df.iloc[:,1].max(), 40)
     fig = px.choropleth_map(df, geojson=geojson, locations=locations, color=color,
                             #color_continuous_scale="Aggrnyl", # Eh
                             #color_continuous_scale="Agsunset", # Eh
@@ -34,33 +36,54 @@ def show(df, geojson, locations, color):
                             #color_continuous_scale="Phase", # Huono
                             range_color=(2, color_max), # NOTE Partikkeleiden raja-arvot väritystä varten
                             map_style="carto-positron",
-                            zoom=3, center = {"lat": 55, "lon": 20},
+                            zoom=3, center = {"lon":10, "lat":55},
                             opacity=0.3,
                             labels={'value':'µg/m3'},
-                            title=df["variable"][0],
-                            animation_frame="leadtime"
+                            title=variable,
+                            width=900,
+                            height=700
+                            #animation_frame="leadtime"
                             )
-    fig.update_layout(margin={"r":0,"t":25,"l":0,"b":0}, title_text=f"{df['variable'][0]} {min(df['leadtime'])}-{max(df['leadtime'])}")
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     fig.update_traces(marker_line_width=0)
-    fig.show()
+    return fig
 
 
 def main():
-    print("Get geojson")
-    geojson_path = "data/geojson/forecast_pm10.geo.json"
-    geojson = geodata.get_geojson(geojson_path)
+    print("Getting geojson...")
+    start = time.perf_counter()
+    #geojson_path = "data/geojson/west-eu.forecast.geo.json"
+    geojson_path = "data/geojson/europe.forecast.geo.json"
+    geojson:GeoJSON = geodata.get_geojson(geojson_path)
+    end = time.perf_counter()
+    print(f"{end-start:.2f}sec")
+
+    print("Getting data...")
+    variable="PM10"
+    start = time.perf_counter()
     df:pd.DataFrame = geodata.get_dataframe(ForecastQuery(
-        variable="PM10",
+        variable=variable,
         time=datetime(2025, 5, 10, 0, 0),
-        leadtime=23,
-        model=None
+        leadtime=0,
+        model=None,
+        limits=None
     ))
-    
+    end = time.perf_counter()
+    print(f"{end-start:.2f}sec")
+
+    print("Getting figure...")
+    start = time.perf_counter()
     locations = df.iloc[:, 0].tolist() # All ids aka every row of first column
     color='value'
+    fig = get_figure(df, geojson, locations, color, variable)
+    end = time.perf_counter()
+    print(f"{end-start:.2f}sec")
 
-    print("Show map")
-    show(df, geojson, locations, color)
+    print("Showing map...")
+    start = time.perf_counter()
+    fig.show()
+    end = time.perf_counter()
+    print(f"{end-start:.2f}sec")
 
 
 
